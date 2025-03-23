26af6893bb10ff42fd2dca251ef4fd3ac8031252
niraj.gupta <niraj.hojai@gmail.com>
2025-03-23 19:22:22 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,27 @@
-
 provider "aws" {
-  region = "eu-west-2"
-}
-
-module "ecr" {
-  source = "./infra/ecr"
+  region = var.region
 }
 
 module "vpc" {
-  source  = "./infra/vpc"
-  aws_region = "eu-west-2"
-  vpc_cidr   = "10.0.0.0/16"
+  source = "./modules/vpc"
+
+  vpc_cidr_block    = var.vpc_cidr_block
+  public_subnets    = var.public_subnets
+  public_subnet_azs = var.public_subnet_azs
+  private_subnets   = var.private_subnets
+  private_subnet_azs = var.private_subnet_azs
 }
 
+module "ecr" {
+  source = "./modules/ecr"
+}
 
 module "eks" {
   source          = "./modules/eks"
-  cluster_name    = "healthcare-app-cluster"
-  private_subnet_ids = module.vpc.private_subnets
-  region         = "eu-west-2"
+  kubernetes_version = "1.21"
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = "AL2_x86_64"
+  instance_types     = ["t3.medium"]
+  aws_region         = "eu-west-2"  # Pass the region
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "The public subnets for the VPC"
+  type        = list(string)
+}
+
+variable "public_subnet_azs" {
+  description = "The Availability Zones for the public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "The private subnets for the VPC"
+  type        = list(string)
+}
+
+variable "private_subnet_azs" {
+  description = "The Availability Zones for the private subnets"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
