0d8e5c0f188e35ba70129e041c5259d4e134cfdf
niraj.gupta <niraj.hojai@gmail.com>
2025-03-23 20:28:15 +0000

    test

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,61 +1,71 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+# Internet Gateway for Public Subnets
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 }
 
+# Public Subnets
 resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = element(var.public_subnets_cidr, count.index)
-  availability_zone       = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.public_subnet_azs, count.index)
   map_public_ip_on_launch = true
 }
 
+# Private Subnets
 resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.private_subnets_cidr, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+  count             = length(var.private_subnets)
+  vpc_id           = aws_vpc.this.id
+  cidr_block       = element(var.private_subnets, count.index)
+  availability_zone = element(var.private_subnet_azs, count.index)
 }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
+# Public Route Table
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.this.id
   }
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
-
+# Associate Public Subnets with Public Route Table
 resource "aws_route_table_association" "public" {
-  count          = 2
+  count          = length(var.public_subnets)
   subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
+  
 }
 
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# NAT Gateway (Required for Private Subnets to Access Internet)
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(aws_subnet.public[*].id, 0)  # Attach to a public subnet
+}
+
+# Private Route Table (Uses NAT Gateway for Internet Access)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+}
+# associate Private Subnets with Private Route Table
 resource "aws_route_table_association" "private" {
-  count          = 2
+  count          = length(var.private_subnets)
   subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = aws_route_table.private.id
 }
