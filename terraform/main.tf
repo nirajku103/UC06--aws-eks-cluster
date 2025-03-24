0f5231d41c78d8ea874b6ab8d59e749eed56e787
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-03-24 13:24:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,54 @@ provider "aws" {
   region = var.region
 }
 
+# OIDC Provider for GitHub
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub's OIDC thumbprint
+  ]
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:nirajku102/UC04-Lamda-deployment:ref:refs/heads/testing-OIDC"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+# Attach Policies to the IAM Role
+resource "aws_iam_role_policy_attachment" "github_actions_ecr" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_terraform" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # Adjust this to least privilege
+}
 module "vpc" {
   source = "./modules/vpc"
 
